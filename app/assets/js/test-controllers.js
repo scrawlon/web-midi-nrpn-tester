cc7928ad0daa50174dbb316a22117d628c2f50d7
scrawlon <scrawlon@gmail.com>
2021-08-01 08:17:16 -0400

    Added option to create a slider, select or radio input controller.

--- app/assets/js/test-controllers.js
+++ app/assets/js/test-controllers.js
@@ -1,53 +1,60 @@
 
 import { sendMidiNRPN } from "./web-midi.js";
 
-const nrpnControllerValuesForm = document.querySelector('form#nrpn-controller-values');
+const nrpnControllerValuesForm = document.querySelector('div#nrpn-controller-values form');
 const nrpnTestControllers = document.querySelector('#nrpn-test-controllers');
 
 function initTestControllerForm() {
+  initTestControllerCreateButtons();
   initTestControllerEvents();
 
-  nrpnControllerValuesForm.addEventListener('submit', function (event) {
-    event.preventDefault();
+  // nrpnControllerValuesForm.addEventListener('click', function (event) {
+  //   event.preventDefault();
 
-    const nrpnControllerValuesInputs = new FormData(event.target);
-    let nrpnControllerValues = {};
+  //   const { target } = event;
 
-    for (var input of nrpnControllerValuesInputs.entries()) {
-      const [key, value] = input;
 
-      nrpnControllerValues[key] = value;
-    };
+  //   if (target.type === 'button') {
+  //     addTestController(target.value);
+  //   }
 
-    addTestController(nrpnControllerValues);
+  //   // const nrpnControllerValuesInputs = new FormData(event.target);
+  //   // let nrpnControllerValues = {};
+
+  //   // for (var input of nrpnControllerValuesInputs.entries()) {
+  //   //   const [key, value] = input;
+
+  //   //   nrpnControllerValues[key] = value;
+  //   // };
+
+  //   // addTestController(nrpnControllerValues);
+  // });
+}
+
+function initTestControllerCreateButtons() {
+  const testControllerCreateButtons = nrpnControllerValuesForm.querySelectorAll('input[type="button"]');
+
+  testControllerCreateButtons.forEach(function (button) {
+    button.addEventListener('click', function (event) {
+      const controllerValues = getControllerValues();
+      const controllerType = button.value;
+
+      addTestController(controllerValues, controllerType);
+    });
   });
 }
 
-function addTestController(nrpnControllerValues) {
-  const { msb, lsb, min, max, channel } = nrpnControllerValues;
-  const controllerId = `nrpn-${Date.now()}`;
-  const controllerHtml = `
-    <div class="component-value" 
-      data-msb="${msb}" 
-      data-lsb="${lsb}" 
-      data-channel="${channel}"> 
-      <div class="close">x</div>
-      <label for="channel-${controllerId}">MIDI Channel</label>
-      <input type="number" name="channel-${controllerId}" id="channel-${controllerId}" min="1" max="16" value="${channel}"/>
-      <label for="${controllerId}">MSB ${msb} : LSB ${lsb} | (${min} - ${max})</label><br>
-      <input 
-        type="range" 
-        name="${controllerId}" 
-        id="${controllerId}" 
-        class="midi-send"
-        min="${min}" 
-        max="${max}" 
-        value="${min}"/>
-      <output name="display" for="${controllerId}">${min}</output>
-    </div>      
-  `;
+function getControllerValues() {
+  const nrpnControllerValuesInputs = new FormData(nrpnControllerValuesForm);
+  let nrpnControllerValues = {};
 
-  nrpnTestControllers.insertAdjacentHTML('beforeend', controllerHtml);
+  for (var input of nrpnControllerValuesInputs.entries()) {
+    const [key, value] = input;
+
+    nrpnControllerValues[key] = value;
+  };
+
+  return nrpnControllerValues;
 }
 
 function initTestControllerEvents() {
@@ -55,7 +62,7 @@ function initTestControllerEvents() {
     const { target: controller } = event;
 
     /* update slider display value in realtime */
-    if (controller && controller.classList.contains('midi-send')) {
+    if (controller && controller.classList.contains('midi-send') && controller.type === 'range') {
       controller.nextElementSibling.value = controller.value;
     }
   });
@@ -83,10 +90,81 @@ function initTestControllerEvents() {
   });
 }
 
+function addTestController(nrpnControllerValues, controllerType) {
+  const { msb, lsb, min, max, channel } = nrpnControllerValues;
+  const controllerId = `nrpn-${Date.now()}`;
+  const controllerHtml = getControllerHtml(controllerType, controllerId, min, max);
+  const componentHtml = `
+    <div class="component-value" 
+      data-msb="${msb}" 
+      data-lsb="${lsb}" 
+      data-channel="${channel}"> 
+      <div class="close">x</div>
+      <label for="channel-${controllerId}">MIDI Channel</label>
+      <input type="number" name="channel-${controllerId}" id="channel-${controllerId}" min="1" max="16" value="${channel}"/>
+      <label for="${controllerId}">MSB ${msb} : LSB ${lsb} | (${min} - ${max})</label><br>
+
+      ${controllerHtml}
+    </div>      
+  `;
+
+  nrpnTestControllers.insertAdjacentHTML('beforeend', componentHtml);
+}
+
+function getControllerHtml(controllerType, controllerId, min, max) {
+  switch (controllerType) {
+    case 'slider':
+      return `
+        <label for="${controllerId}">SEND MIDI</label>
+        <input type="range" name="${controllerId}" id="${controllerId}" class="midi-send" min="${min}"  max="${max}" value="${min}"/>
+        <output name="display" for="${controllerId}">${min}</output>              
+      `;
+    case 'select':
+      return `
+        <label for="${controllerId}">SEND MIDI</label>
+        <select name="${controllerId}" id="${controllerId}" class="midi-send">
+        ${(() => {
+          let optionsHtml = '';
+
+          for (let i = min; i <= max; i++) {
+            optionsHtml += `
+              <option value="${i}">${i}</option>
+            `;
+          }
+
+          return optionsHtml;
+        })()
+        }
+        </select >
+    `;
+    case 'radio':
+      return `
+        <label for="${controllerId}">SEND MIDI</label>
+        ${(() => {
+          let radioGroupHtml = '';
+
+          for (let i = min; i <= max; i++) {
+            radioGroupHtml += `
+              <label>
+                <input type="radio" id="${controllerId}-choice-${i}" name="${controllerId}-choice" value="${i}" ${i === min ? "checked" : ""}>
+                ${i}
+              </label>
+            `;
+          }
+
+          return radioGroupHtml;
+        })()
+        }
+      `;
+  };
+}
+
 function sendMidiControllerValues(controller) {
   const component = controller.closest('.component-value');
   const channel = component.querySelector('[name^="channel"]');
   const { msb, lsb } = component.dataset;
+
+  console.log({ controller, channel, msb, lsb });
 
   if (channel, msb, lsb) {
     sendMidiNRPN(parseInt(channel.value), parseInt(msb), parseInt(lsb), parseInt(controller.value));
